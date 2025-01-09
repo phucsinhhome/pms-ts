bfbbdb4990272fbb929124e981b6c7c7d6504bbe
Minh Trần <wisekingtran@gmail.com>
2025-01-09 15:25:42 +0700

    Update OrderManager.tsx

--- src/Components/OrderManager.tsx
+++ src/Components/OrderManager.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
-import { beginOfDay, formatISODate, formatISODateTime, utcToHourMinute } from "../Service/Utils";
+import { formatISODate, formatISODateTime, utcToHourMinute } from "../Service/Utils";
 import { Chat, DEFAULT_PAGE_SIZE } from "../App";
 import { fetchUpcomingOrders } from "../db/order";
 import { Button, Modal, TextInput } from "flowbite-react";
@@ -76,8 +76,9 @@ export const OrderManager = (props: OrderManagerProps) => {
 
   const fetchOrders = async () => {
     var today = new Date()
-    var yesterday = new Date(today.setDate(today.getDate() - 1))
-    var fromTime = formatISODateTime(yesterday)
+    today.setDate(today.getDate() - 1)
+    today.setHours(0, 0, 0, 0)
+    var fromTime = formatISODateTime(today)
     console.info(`Fetch upcoming order after ${fromTime}`)
 
     const ordersData = await fetchUpcomingOrders(fromTime, activeListOpt, pagination.pageNumber, pagination.pageSize)
@@ -211,61 +212,58 @@ export const OrderManager = (props: OrderManagerProps) => {
   }
 
   return (
-    <div className="h-full pt-3">
+    <div className="h-full pt-3 relative">
       <div className="flex flex-row items-center w-full pb-4 px-2 space-x-3">
         <Button onClick={findTheInvoice}>Copy Link</Button>
         <div onClick={changeListOpt} className="px-2 font-mono text-sm border rounded-sm shadow-sm bg-slate-200">
           {activeListOpt.toUpperCase()}
         </div>
       </div>
-      <div className="h-3/5 px-2 overflow-hidden">
-        <div className="flex flex-col space-y-2">
-          {orders.map((order) => {
-            return (
-              <div
-                className="flex flex-row items-center border border-gray-300 shadow-2xl rounded-md px-2 bg-white dark:bg-slate-500 "
-                key={order.orderId}
-              >
-                <div className="w-full">
-                  <div className="grid grid-cols-1">
-                    <div className="flex flex-row">
-                      <Link
-                        to={order.id + "/" + props.chat.id}
-                        state={{ pageNumber: pagination.pageNumber, pageSize: pagination.pageSize }}
-                        className="font-medium text-blue-600 hover:underline dark:text-blue-500 overflow-hidden"
-                      >
-                        {order.guestName}
-                      </Link>
-                    </div>
-                    <div className="flex flex-row text-sm space-x-3">
-                      <div className="flex flex-row items-center rounded-sm">
-                        <HiMail />
-                        <span className="font font-mono text-gray-500 text-[12px]">{utcToHourMinute(order.startTime)}</span>
-                      </div>
-                      {order.expectedTime ? <div className="flex flex-row items-center rounded-sm">
-                        <GiMeal />
-                        <span className="font font-mono text-gray-500 text-[12px]">{utcToHourMinute(order.expectedTime)}
-                        </span></div> : <></>
-                      }
-                      {order.invoiceId ? <div className="flex flex-row items-center rounded-sm">
-                        <GiHouse />
-                        <span className="font font-mono text-[12px]">{order.rooms}
-                        </span></div> : <></>}
-                    </div>
+      <div className="flex flex-col px-2 overflow-hidden space-y-1.5">
+        {orders.map((order) => {
+          return (
+            <div
+              className="flex flex-row items-center border border-gray-300 shadow-2xl rounded-md px-2 bg-white dark:bg-slate-500 "
+              key={order.orderId}
+            >
+              <div className="w-full">
+                <div className="grid grid-cols-1">
+                  <div className="flex flex-row">
+                    <Link
+                      to={order.id + "/" + props.chat.id}
+                      state={{ pageNumber: pagination.pageNumber, pageSize: pagination.pageSize }}
+                      className="font-medium text-blue-600 hover:underline dark:text-blue-500 overflow-hidden"
+                    >
+                      {order.guestName}
+                    </Link>
                   </div>
-                </div>
-                <div className="pl-0.2 pr-1">
-                  <div className="bg-zinc-200 rounded-md py-0.5 w-24 text-center">
-                    <span className={"font font-mono font-bold " + OrderStatus[order.status as SK]}>{order.status}</span>
+                  <div className="flex flex-row text-sm space-x-3">
+                    <div className="flex flex-row items-center rounded-sm">
+                      <HiMail />
+                      <span className="font font-mono text-gray-500 text-[12px]">{utcToHourMinute(order.startTime)}</span>
+                    </div>
+                    {order.expectedTime ? <div className="flex flex-row items-center rounded-sm">
+                      <GiMeal />
+                      <span className="font font-mono text-gray-500 text-[12px]">{utcToHourMinute(order.expectedTime)}
+                      </span></div> : <></>
+                    }
+                    {order.invoiceId ? <div className="flex flex-row items-center rounded-sm">
+                      <GiHouse />
+                      <span className="font font-mono text-[12px]">{order.rooms}
+                      </span></div> : <></>}
                   </div>
                 </div>
               </div>
-            )
-          })}
-        </div>
+              <div className="pl-0.2 pr-1">
+                <div className="bg-zinc-200 rounded-sm py-0.5 w-24 text-center">
+                  <span className={"font font-mono " + OrderStatus[order.status as SK]}>{order.status}</span>
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
-      <nav className="flex items-center justify-between pt-4" aria-label="Table navigation">
-        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Showing <span className="font-semibold text-gray-900 dark:text-white">{pagination.pageSize * pagination.pageNumber + 1}-{pagination.pageSize * pagination.pageNumber + pagination.pageSize}</span> of <span className="font-semibold text-gray-900 dark:text-white">{pagination.totalElements}</span></span>
+      <nav className="flex items-center justify-between mt-2 px-2 absolute bottom-1" aria-label="Table navigation">
         <ul className="inline-flex items-center -space-x-px">
           <li onClick={() => handlePaginationClick(pagination.pageNumber - 1)} className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
             <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>

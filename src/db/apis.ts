7e3375fa08deea721dad90d08ae55246cd96286a
Minh Trần <wisekingtran@gmail.com>
2025-07-07 08:04:08 +0700

    Update apis.ts

--- src/db/apis.ts
+++ src/db/apis.ts
@@ -19,119 +19,132 @@ const configureAccessToken = (config: InternalAxiosRequestConfig) => {
 
 // reportApi
 const reportApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_PROFIT_SERVICE_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_PROFIT_SERVICE_ENDPOINT}`,
+    withCredentials: true
 });
-reportApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// reportApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // expenseApi
 const expenseApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_EXPENSE_SERVICE_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_EXPENSE_SERVICE_ENDPOINT}`,
+    withCredentials: true
 });
-expenseApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// expenseApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // invoiceApi
 const invoiceApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_INVOICE_SERVICE_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_INVOICE_SERVICE_ENDPOINT}`,
+    withCredentials: true
 });
-invoiceApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// invoiceApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // inventoryApi
 const inventoryApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_INVENTORY_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_INVENTORY_ENDPOINT}`,
+    withCredentials: true
 });
-inventoryApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// inventoryApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // productApi
 const productApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_PRODUCT_SERVICE_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_PRODUCT_SERVICE_ENDPOINT}`,
+    withCredentials: true
 });
-productApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// productApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // productGroupApi
 const productGroupApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_PRODUCT_GROUP_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_PRODUCT_GROUP_ENDPOINT}`,
+    withCredentials: true
 });
-productGroupApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// productGroupApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // orderApi
 const orderApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_ORDER_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_ORDER_ENDPOINT}`,
+    withCredentials: true
 });
-orderApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// orderApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // reservationApi
 const reservationApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_RESERVATION_SERVICE_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_RESERVATION_SERVICE_ENDPOINT}`,
+    withCredentials: true
 });
-reservationApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// reservationApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // reservationExtractApi
 const reservationExtractApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_RESERVATION_EXTRACT_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_RESERVATION_EXTRACT_ENDPOINT}`,
+    withCredentials: true
 });
-reservationExtractApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// reservationExtractApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // supplierApi
 const supplierApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_SUPPLIER_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_SUPPLIER_ENDPOINT}`,
+    withCredentials: true
 });
-supplierApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// supplierApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // tourApi
 const tourApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_TOUR_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_TOUR_ENDPOINT}`,
+    withCredentials: true
 });
-tourApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// tourApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 // tourRequestApi
 const tourRequestApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_TOUR_REQUEST_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_TOUR_REQUEST_ENDPOINT}`,
+    withCredentials: true
 });
-tourRequestApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// tourRequestApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 // configApi
 const configApi: AxiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_CONFIG_ENDPOINT}`
+    baseURL: `${process.env.REACT_APP_CONFIG_ENDPOINT}`,
+    withCredentials: true
 });
-configApi
-    .interceptors
-    .request
-    .use(configureAccessToken, (error) => Promise.reject(error));
+// configApi
+//     .interceptors
+//     .request
+//     .use(configureAccessToken, (error) => Promise.reject(error));
 
 export {
     reportApi,

b274bb41f05bf01383019432b43fca8dfb5b4d51
Minh Trần <wisekingtran@gmail.com>
2026-02-08 00:18:37 +0700

    Update the bank transfer

--- src/Components/InvoiceEditor.tsx
+++ src/Components/InvoiceEditor.tsx
@@ -45,124 +45,6 @@ import { listAllPGroups } from "../db/pgroup";
 import { PGroup } from "./PGroupManager";
 import { PERMISSION_INVOICE_ASSIGN } from "../db/permission";
 
-const paymentIcons = [
-  {
-    id: "cash",
-    src: (
-      <svg
-        className="h-6 w-6 text-gray-800 dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="currentColor"
-        viewBox="0 0 24 24"
-      >
-        {" "}
-        <path
-          fillRule="evenodd"
-          d="M7 6a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-2v-4a3 3 0 0 0-3-3H7V6Z"
-          clipRule="evenodd"
-        />{" "}
-        <path
-          fillRule="evenodd"
-          d="M2 11a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-7Zm7.5 1a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5Z"
-          clipRule="evenodd"
-        />{" "}
-        <path d="M10.5 14.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z" />
-      </svg>
-    ),
-  },
-  {
-    id: "creditCard",
-    src: (
-      <svg
-        className="h-6 w-6 text-gray-800 dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"
-        />
-      </svg>
-    ),
-  },
-  {
-    id: "momo",
-    src: (
-      <svg
-        className="h-6 w-6 text-gray-800 dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"
-        />
-      </svg>
-    ),
-  },
-  {
-    id: "paypal",
-    src: (
-      <svg
-        className="h-6 w-6 text-gray-800 dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"
-        />
-      </svg>
-    ),
-  },
-  {
-    id: "bankTransfer",
-    src: (
-      <svg
-        className="h-6 w-6 text-gray-800 dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"
-        />
-      </svg>
-    ),
-  },
-];
-
 const roomIcons = [
   {
     id: "r1",
@@ -1402,38 +1284,26 @@ export const InvoiceEditor = (props: InvoiceProps) => {
             </div>
 
             <div className="-mx-3 mb-3 flex flex-wrap">
-              <div className="flex flex-row items-center px-3">
-                <div>
-                  {
-                    paymentIcons.find(
-                      (ic) => ic.id === selectedPaymentMethod.id,
-                    )?.src
-                  }
-                </div>
-                <Label
-                  id="paymentMethod"
-                  value={selectedPaymentMethod.name}
-                  className="pr-2"
-                />
-                <svg
-                  className="h-[16px] w-[16px] cursor-pointer text-gray-800 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  id="paymentMethod"
+              <div className="flex flex-row justify-items-center px-3">
+                <Button
+                  size="sm"
+                  color="green"
+                  className="h-11 w-16"
                   onClick={selectPaymentMethod}
                 >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
-                  />
-                </svg>
+                  {invoice.paymentMethod ? (
+                      <img
+                      src={
+                        paymentMethods.find(
+                          (ic) => ic.id === selectedPaymentMethod.id,
+                        )?.srcLargeImg
+                      }
+                      alt=""
+                    />
+                  ) : (
+                    "Pay"
+                  )}
+                </Button>
               </div>
               <div className="flex flex-row items-center px-3">
                 <Label
@@ -1472,26 +1342,36 @@ export const InvoiceEditor = (props: InvoiceProps) => {
           </div>
         </form>
 
-        <div className="flex w-full flex-row space-x-2 px-1">
+        <div className="flex flex-row w-full space-x-3 justify-center">
           <Button
             size="xs"
             color="green"
             onClick={() => editItem(defaultEmptyItem.origin)}
           >
-            <MdAssignmentAdd size="1.5em" className="mr-2" /> Add
+            <div className="flex flex-col items-center">
+              <MdAssignmentAdd size="1.5em" /><span>Add</span>
+            </div>
           </Button>
           <Button size="xs" color="green" onClick={handleMenuClick}>
-            <MdAssignmentAdd size="1.5em" className="mr-2" /> Menu
+            <div className="flex flex-col items-center">
+            <MdAssignmentAdd size="1.5em"/><span>Menu</span>
+            </div>
           </Button>
-          <Button size="xs" color="green" onClick={showViewInv}>
-            <FaEye size="1.5em" className="mr-2" /> View
-          </Button>
+          <Button size="xs" color="green" onClick={showViewInv} disabled={!invoice.paymentMethod}>
+            <div className="flex flex-col items-center">
+              <FaEye size="1.5em"/><span>View</span>
+              </div>
+            </Button>
           <Button size="xs" color="green" onClick={handleSaveInvoice}>
-            <HiSave size="1.5em" className="mr-2" /> Save
+            <div className="flex flex-col items-center">
+            <HiSave size="1.5em" /><span>Save</span>
+            </div>
           </Button>
           <Button size="xs" color="green" onClick={() => navigate(-1)}>
-            <IoMdArrowBack size="1.5em" className="mr-2" />
+            <div className="flex flex-col items-center">
+            <IoMdArrowBack size="1.5em" />
             <span>Back</span>
+            </div>
           </Button>
         </div>
 
